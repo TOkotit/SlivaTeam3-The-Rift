5dd7f453691ce2752e395668aedde9013feb6e64
TOkotit <105845895+TOkotit@users.noreply.github.com>
2026-02-09 19:42:05 +0500

    Scenes и UIRootView
    Добавил файлы с прошлого проекта. По любому надо переделывать под DI

---
+++ The Rift/Assets/Scripts/Root/Scenes.cs
@@ -0,0 +1,9 @@
+﻿namespace Root
+{
+    public class Scenes
+    {
+        public const string BOOT = "Boot";
+        public const string MENU = "Menu";
+        public const string GAMEPLAY = "Gameplay";
+    }
+}
---
+++ The Rift/Assets/Scripts/Root/UIRootView.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+// Перенёс из другого проекта файл, скорей всего надо будет переделывать под DI
+
+namespace Root
+{
+    public class UIRootView :  MonoBehaviour
+    {
+
+        [SerializeField] private GameObject _loadingScreen;
+        [SerializeField] private Transform _uiSceneContainer;
+
+        private void Awake()
+        {
+            HideLoadingScreen();
+        }
+
+        public void ShowLoadingScreen()
+        {
+            _loadingScreen.SetActive(true);
+        }
+
+        public void HideLoadingScreen()
+        {
+            _loadingScreen.SetActive(false);
+        }
+        
+        public void AttachSceneUI(GameObject sceneUI)
+        {
+            ClearSceneUI();
+
+            sceneUI.transform.SetParent(_uiSceneContainer, false);
+        }
+
+        private void ClearSceneUI()
+        {
+            var childCount = _uiSceneContainer.childCount;
+            for (var i = 0; i < childCount; i++)
+            {
+                Destroy(_uiSceneContainer.GetChild(i).gameObject);
+            }
+        }
+    }
+}
