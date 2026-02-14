73e5736c966b4829b2db86f26198ad63c59af257
zyuzyakin <zyuzyakinIvan06@gmail.com>
2026-02-14 17:45:12 +0500

    создал префабы окон в русурсах, начинаю интегрировать UI в точки входа и переключение сцен

--- The Rift/Assets/Scripts/Game/Gameplay/View/UI/ScreenGameplay/ScreenGameplayBinder.cs
+++ The Rift/Assets/Scripts/Game/Gameplay/View/UI/ScreenGameplay/ScreenGameplayBinder.cs
@@ -8,20 +8,29 @@ namespace Game.Gameplay.View.UI
     public class ScreenGameplayBinder : WindowBinder<ScreenGameplayViewModel>
     {
         [SerializeField] private Button _btnGoToMainMenu;
+        [SerializeField] private Button _btnPopupA;
+
 
         private void OnEnable()
         {
             _btnGoToMainMenu?.onClick.AddListener(OnGoToMainMenuButtonClicked);
+            _btnPopupA?.onClick.AddListener(OnPopupAButtonClicked);
         }
 
         private void OnDisable()
         {
             _btnGoToMainMenu?.onClick.RemoveListener(OnGoToMainMenuButtonClicked);
+            _btnPopupA?.onClick.RemoveListener(OnPopupAButtonClicked);
         }
 
         private void OnGoToMainMenuButtonClicked()
         {
             ViewModel.RequestGoToMainMenu();
+        }
+        
+        private void OnPopupAButtonClicked()
+        {
+            ViewModel.RequestOpenPopupA();
         }
     }
 }
---
+++ The Rift/Assets/Scripts/Game/MainMenu/View/UI/ScreenMainMenu/ScreenMainMenuBinder.cs
@@ -0,0 +1,46 @@
+﻿using Game.UI;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Game.MainMenu.View.UI.ScreenMainMenu
+{
+    public class ScreenMainMenuBinder : WindowBinder<ScreenMainMenuViewModel>
+    {
+        [SerializeField] private Button _btnPlay;
+        [SerializeField] private Button _btnContinue;
+        [SerializeField] private Button _btnOptions;
+        [SerializeField] private Button _btnExit;
+
+
+        private void OnEnable()
+        {
+            _btnPlay?.onClick.AddListener(OnPlayButtonClicked);
+        }
+
+        private void OnDisable()
+        {
+
+        }
+
+        private void OnPlayButtonClicked()
+        {
+            ViewModel.RequestPlay();
+        }
+        
+        private void OnContinueButtonClicked()
+        {
+            
+        }
+        
+        private void OnOptionsButtonClicked()
+        {
+            
+        }
+        
+        private void OnExitButtonClicked()
+        {
+            
+        }
+        
+    }
+}
