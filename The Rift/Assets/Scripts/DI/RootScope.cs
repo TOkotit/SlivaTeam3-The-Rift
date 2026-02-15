6c990bb337caf4aba38f6d5aaeee33f3ef5e6b1e
TOkotit <105845895+TOkotit@users.noreply.github.com>
2026-02-15 17:25:09 +0500

    Изменил сцену Владимира (извините) и добавил большой комментарий в RootScope

--- The Rift/Assets/Scripts/DI/RootScope.cs
+++ The Rift/Assets/Scripts/DI/RootScope.cs
@@ -2,6 +2,7 @@
 using UIRoot;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using Utils;
 using VContainer;
 using VContainer.Unity;
@@ -44,7 +45,24 @@ namespace DI
             Debug.Log("GameplayScope.Configure called");
             builder.Register<IGameManager, GameManager>(Lifetime.Singleton);
             
-            builder.RegisterEntryPoint<Root.EntryPoint>(Lifetime.Singleton);
+            
+            
+            var activeSceneName = SceneManager.GetActiveScene().name;
+            
+            // Короче, если вам нужен какой-то контейнер на сцене, то вот этот базовый RootScope должен быть либо инициализирован на прошлой сцене
+            // Либо он должен быть на вашей сцене. И вы должны указать что он наследуется от RootScope чтобы все данные отсюда тоже перешли в ваш контейнер
+            // в игре это всё при запуске создаётся и работает нормально, не надо самому всё это запихивать, но работая на одной сцене вам придётся ручками добавить контейнеры
+            // Дальше я написал код который игнорит запуск энтри поинт для того, чтобы при запуске ваша сцена на меню не переключилась.
+            // если вы не в boot, то всё инициализируется, но в меню вы не перейдёте
+            
+            if (Application.isEditor && !string.Equals(activeSceneName, "Boot", System.StringComparison.OrdinalIgnoreCase))
+            {
+                Debug.Log($"Skipping RegisterEntryPoint — running in editor and active scene is '{activeSceneName}' (not 'Boot').");
+            }
+            else
+            {
+                builder.RegisterEntryPoint<Root.EntryPoint>(Lifetime.Singleton);
+            }
         }
     }
 }
