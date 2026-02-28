9a10ef8543f28f4beebd504cae6d8af77b9efd15
zyuzyakin <zyuzyakinIvan06@gmail.com>
2026-02-28 17:44:37 +0500

    behavior package

---
+++ The Rift/Assets/Scripts/Entity/Enemy/Warrior/Warrior.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace Entity.Enemy.Warrior
+{
+    public class Warrior : Enemy
+    {
+        //Область в которой противник засекает персонажа, не видя напрямую
+        [SerializeField] private Transform _proximityAreaCenter;
+        [SerializeField] private float _proximityAreaRadius;
+        
+        //Поле зрения в котором противник видит персонажа, намного больше proximityArea
+        [SerializeField] private GameObject _sightArea;
+        
+        
+        
+        
+        
+        
+        public void FindCharacter()
+        {
+            
+        }
+    }
+}
