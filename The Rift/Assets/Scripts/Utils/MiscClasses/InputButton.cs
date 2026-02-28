1d359cccc783f99d9140fcb62b236d3315e374e3
Sp1ke <97825185+SpikeTheSpider@users.noreply.github.com>
2026-03-01 02:30:15 +0500

    InputButton
    Класс для общего описания входящего сигнала для мыши и клавиатуры

---
+++ The Rift/Assets/Scripts/Utils/MiscClasses/InputButton.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Unity.VisualScripting;
+using UnityEngine.InputSystem;
+
+namespace Utils.MiscClasses
+{
+    [Serializable]
+    public struct InputButton
+    {
+        public enum DeviceType { Keyboard, Mouse }
+        public DeviceType device;
+        public Key key;          
+        public MouseButton mouse;
+        
+        public static InputButton Keyboard(Key key) => new InputButton { device = DeviceType.Keyboard, key = key }; 
+        public static InputButton Mouse(MouseButton mouse) => new InputButton { device = DeviceType.Mouse, mouse = mouse };
+    }
+}
