416daf5c5fa84a28595fd2bdcc343f4aa89a9dd9
zyuzyakin <zyuzyakinIvan06@gmail.com>
2026-03-09 15:43:10 +0500

    Enemy Stats Const

---
+++ The Rift/Assets/Scripts/Entity/BC.cs
@@ -0,0 +1,15 @@
+﻿namespace Entity
+{
+    /// <summary>
+    /// Класс для работы с балансом с помощью у.е. (условных единиц)
+    /// </summary>
+    public static class BC
+    {
+        public static float Damage = 20;
+        public static float Health = 200;
+        public static float Speed = 5;
+        
+        public static float CD = 1;
+        public static float AtkSpeed = 1;
+    }
+}
--- The Rift/Assets/Scripts/Entity/Enemy/Enemy.cs
+++ The Rift/Assets/Scripts/Entity/Enemy/Enemy.cs
@@ -6,8 +6,7 @@ namespace Entity.Enemy
 {
     public class Enemy : Character
     {
-        [Inject] 
-        private EnemyModel _enemyModel;
+        [Inject] private EnemyModel _enemyModel;
         public override DamagableModel Damagable => _enemyModel;
         public EnemyModel EnemyModel => _enemyModel; 
     }
