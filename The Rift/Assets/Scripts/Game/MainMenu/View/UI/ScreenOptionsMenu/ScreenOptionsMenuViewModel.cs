04e1e0bdb05978cd699bad01bfe23d4a51228198
zyuzyakin <zyuzyakinIvan06@gmail.com>
2026-03-29 23:03:40 +0500

    options screen classes

---
+++ The Rift/Assets/Scripts/Game/MainMenu/View/UI/ScreenOptionsMenu/ScreenOptionsMenuViewModel.cs
@@ -0,0 +1,24 @@
+﻿using Game.UI;
+using Systems;
+using Utils;
+using VContainer;
+
+namespace Game.MainMenu.View.UI.ScreenOptionsMenu
+{
+    public class ScreenOptionsMenuViewModel : WindowViewModel
+    {
+        public override string Id =>  "ScreenOptionsMenu";
+        
+        private readonly MainMenuUIManager _uiManager;
+        private readonly IGameManager _gameManager;
+        private readonly ICoroutineRunner _coroutines;
+        
+        
+        public ScreenOptionsMenuViewModel(MainMenuUIManager uiManager, IObjectResolver container)
+        {
+            _uiManager = uiManager;
+            _gameManager =  container.Resolve<IGameManager>();
+            _coroutines = container.Resolve<ICoroutineRunner>();
+        }
+    }
+}
